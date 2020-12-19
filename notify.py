f583452533ffcb08458dbf62db7abdb23c49b920
kavishdev <devar.kavish@gmail.com>
2020-12-19 19:21:19 +0530

    v. 0.1.1

--- notify.py
+++ notify.py
@@ -1,8 +1,17 @@
+
 import platform
-from main import cases
+from main import cases,deaths,recovered,active,confirmed
+import pyttsx3
+from notifypy import Notify
+
+
 
 appname="Covid Cases in Haryana"
-iconfile = './favicon.png'
+iconfile = './icon.png'
+
+c="In Haryana,"+" people died are "+str(deaths)+". Active cases are "+str(active)+". Patients recovered are "+str(recovered)+". Total cases confirmed are "+str(confirmed)
+print(c)
+
 
 c_platform=platform.system()
 
@@ -11,6 +20,16 @@ if c_platform == 'Linux':
    notify2.init(appname)
    notification = notify2.Notification(appname, cases)
    notification.show()
-elif c_platform == 'Darwin':
-   import pync
-   pync.notify(cases,title=appname)
+
+else:
+   notification = Notify(default_notification_title="Function Message", default_application_name="Covid-19 Cases", default_notification_icon="./icon.png", default_notification_audio="./sound.wav")
+   notification.icon='./icon.png'
+   notification.message = c
+   notification.send()
+
+
+
+engine = pyttsx3.init()
+engine.setProperty('rate', 215)
+#engine.say(c)
+#engine.runAndWait()
